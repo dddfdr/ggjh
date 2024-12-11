22695060bc08b2246538aeaacc2ab125f7322954
dddfdr <133726713+dddfdr@users.noreply.github.com>
2024-12-12 07:41:43 +0800

    Update Hanime1.js

--- uzVideo-extensions/refs/heads/main/vod/js/Hanime1.js
+++ uzVideo-extensions/refs/heads/main/vod/js/Hanime1.js
@@ -1,5 +1,5 @@
 const appConfig = {
-    _webSite: 'https://hanime1.me/#',
+    _webSite: 'https://hanime1.me',
     /**
      * 网站主页，uz 调用每个函数前都会进行赋值操作
      * 如果不想被改变 请自定义一个变量
@@ -11,7 +11,7 @@ const appConfig = {
         this._webSite = value
     },
 
-    _uzTag: '',
+    _uzTag: 'https://hanime1.me/search?genre=裏番',
     /**
      * 扩展标识，初次加载时，uz 会自动赋值，请勿修改
      * 用于读取环境变量
